dc73725d6b1ba64f65dd1d56e3d549fdce5e97e4
jaerrib <jaerrib@tutanota.com>
2024-09-02 05:55:00 -0400

    Update deprecated assertion test

--- books/tests.py
+++ books/tests.py
@@ -33,22 +33,22 @@ class BookTests(TestCase):
         )
 
     def test_book_listing(self):
-        self.assertEquals(f"{self.book.title}", "Harry Potter")
-        self.assertEquals(f"{self.book.author}", "JK Rowling")
-        self.assertEquals(f"{self.book.price}", "25.00")
+        self.assertEqual(f"{self.book.title}", "Harry Potter")
+        self.assertEqual(f"{self.book.author}", "JK Rowling")
+        self.assertEqual(f"{self.book.price}", "25.00")
 
 
     def test_book_list_view_for_logged_in_user(self):
         self.client.login(email="reviewuser@email.com", password="testpass123")
         response = self.client.get(reverse('book_list'))
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Harry Potter")
         self.assertTemplateUsed(response, 'books/book_list.html')
 
     def test_book_list_view_for_logged_out_user(self):
         self.client.logout()
         response = self.client.get(reverse('book_list'))
-        self.assertEquals(response.status_code, 302)
+        self.assertEqual(response.status_code, 302)
         self.assertRedirects(
             response, "%s?next=/books/" % (reverse("account_login"))
         )
@@ -60,8 +60,8 @@ class BookTests(TestCase):
         self.user.user_permissions.add(self.special_permission)
         response = self.client.get(self.book.get_absolute_url())
         no_response = self.client.get("/books/12345")
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(no_response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, "Harry Potter")
         self.assertContains(response, "An excellent review")
         self.assertTemplateUsed(response, 'books/book_detail.html')
